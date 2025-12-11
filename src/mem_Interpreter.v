fbe8233bb4d5edee3ed903a5d149c922c322cace
Khaled <137321744+khaled759@users.noreply.github.com>
2025-12-11 17:50:38 +0200

    fix Sp offset bug

--- src/mem_Interpreter.v
+++ src/mem_Interpreter.v
@@ -3,7 +3,7 @@ module mem_Interpreter #(
     parameter interrupt_offset = 0, // 0 -> 19(20)
     parameter Instruction_offset = 20, // 20 -> 99(80)
     parameter data_offset = 100, // 100 -> 199(100)
-    parameter SP_offset = 200 // 200 --> 255(56)
+    parameter SP_offset = 0 // 200 --> 255(56)
 ) (
     input wire [Addr_width-1:0] interrupt, Instruction, data, SP,
     input wire [1:0] sel,
